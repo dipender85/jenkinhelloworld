51bd577ad5533c837fb9821e692017191df10723
dipender85 <rawat.dipendersingh@gmail.com>
2017-12-26 23:27:49 +0000

    added jenkin hello world program

---
+++ JenkinsFirstProgram.java
@@ -0,0 +1,8 @@
+public class JenkinsFirstProgram {
+/* This is my first jenkin java program.
+* This will print 'Hello World' as the output
+*/
+public static void main(String []args) {
+System.out.println("Hello World of Jenkins 2"); // prints Hello World
+}
+}
